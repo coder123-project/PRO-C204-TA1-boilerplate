3a2744bf541352949a1f6d30fe8be8d54c156320
whitehatjr <whitehatjr@whitehatjr.com>
2021-07-03 06:36:40 +0530

    initial commit

---
+++ client.py
@@ -0,0 +1,46 @@
+
+import socket
+from tkinter import *
+from  threading import Thread
+from PIL import ImageTk, Image
+
+screen_width = None
+screen_height = None
+
+SERVER = None
+PORT = None
+IP_ADDRESS = None
+
+
+canvas1 = None
+
+playerName = None
+nameEntry = None
+nameWindow = None
+
+
+
+#Teacher write code here for askPlayerName()
+
+
+
+
+def setup():
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    PORT  = 5000
+    IP_ADDRESS = '127.0.0.1'
+
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.connect((IP_ADDRESS, PORT))
+
+
+    # Creating First Window
+    askPlayerName()
+
+
+
+
+setup()
